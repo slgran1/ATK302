612f209b6ba7943cfcbbdafe12b3a373995a9083
slgran1 <54331537+slgran1@users.noreply.github.com>
2019-08-22 14:21:18 -0500

    a;owiefj

--- p5/circle/sketch.js
+++ p5/circle/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   // put setup code here
-  createCanvas(500, 500);
+  createCanvas(200, 200);
 }
 
 function draw() {
   // put drawing code here
 
-  rect(mouseX, mouseY, 10, 950) ;
+  rect(mouseX, mouseY, 10, 10) ;
 }
