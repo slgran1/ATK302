52ed17d8f83977c49e0f5433c3ae3b8c5e5b1374
slgran1 <54331537+slgran1@users.noreply.github.com>
2019-09-17 13:52:44 -0500

    k

--- p5/Week5SoundToggle/sketch.js
+++ p5/Week5SoundToggle/sketch.js
@@ -1,7 +1,27 @@
+var song1;
+
+function preload() {
+  song1 = loadSound('assets/piano.mp3');
+}
+
 function setup() {
   // put setup code here
+
 }
 
 function draw() {
   // put drawing code here
+
+}
+
+
+function mouseReleased () {
+      
+      if (song1.isPlaying()) {
+        song1.pause() ;
+      } else {
+        song1.play();
+
+      }
+
 }
