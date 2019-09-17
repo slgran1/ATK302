78dd1fb6c75808c62b0619c8625a7d95fdc965bd
slgran1 <54331537+slgran1@users.noreply.github.com>
2019-09-17 14:27:47 -0500

    oi

---
+++ p5/Week5StateMouse/sketch.js
@@ -0,0 +1,39 @@
+var myState = 0;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+}
+
+function draw() {
+  // put drawing code here
+
+
+  switch (myState) {
+
+    case 0:
+      background(255, 0, 0);
+      text("case 0", 100, 100);
+      break;
+
+    case 1:
+      background(0, 255, 0);
+      text("case 1", 100, 100);
+      break;
+
+    case 2:
+      background(0, 0, 255);
+      text("case 2", 100, 100);
+      break;
+
+  }
+
+}
+
+function mouseReleased() {
+  myState ++
+
+  if (myState > 2) {
+    myState = 0;
+  }
+}
