8b92008e4f8a5f2d4df5a8c75e694e747b9e07eb
slgran1 <54331537+slgran1@users.noreply.github.com>
2019-10-01 14:09:36 -0500

    vroom vroom mf-er

--- p5/stop1/sketch.js
+++ p5/stop1/sketch.js
@@ -1,5 +1,6 @@
 var myState = 0;
 var timer = 200;
+var x = 0;
 
 
 function setup() {
@@ -12,7 +13,7 @@ function setup() {
 function draw() {
   // put drawing code here
   background(100);
-  fill('#F7FF3C');
+  fill('#F7FF3C'); //stoplight box
   rect(width / 2, height / 2, 150, 400);
 
   switch (myState) {
@@ -24,6 +25,8 @@ function draw() {
       fill('green');
       ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
 
+      doTimer(1);
+      velocity = 5;
 
       break;
 
@@ -36,6 +39,9 @@ function draw() {
       fill('yellow');
       ellipse(width / 2, height / 2, 100, 100); // middle
 
+      doTimer(2);
+      velocity = 2;
+
       break;
 
     case 2: // red state
@@ -46,8 +52,27 @@ function draw() {
       ellipse(width / 2, height / 2, 100, 100); // middle
       ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
 
+      doTimer(0);
+      velocity = 0;
+
       break;
   }
 
+  fill('#7B0000');
+  rect(x, height - 100, 80, 50);
+  x = x + 5;
 
+  if (x > width) {
+    x = 0;
+  }
+
+}
+
+
+function doTimer(variableState) {
+  timer--;
+  if (timer <= 0) {
+    myState = variableState;
+    timer = 200;
+  }
 }
